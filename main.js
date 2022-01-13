fdbbc5da3c9f47c25b5a677ecbfc8ae4d381dc20
David Clark <david.clark.1043@gmail.com>
2022-01-13 11:07:07 -0600

    Added set() usage

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-const phones = [
-    {
+const phones = new Set()
+
+const iPhone = {
         id: 1,
         name: "iPhone",
         maker: "Apple",
         operatingSystem: "iOS",
         price: 900,
         weight: 1.2
-    },
-    {
-        id: 2,
-        name: "Galaxy",
-        maker: "Samsung",
-        operatingSystem: "Android",
-        price: 600,
-        weight: 1.4
-    }
-]
+}
+const galaxy = {
+    id: 2,
+    name: "Galaxy",
+    maker: "Samsung",
+    operatingSystem: "Android",
+    price: 600,
+    weight: 1.4
+}
+
+phones.add(iPhone);
+phones.add(galaxy);
+phones.add(iPhone);
+
+
 // define function that sets the price of a phone object in the inventory map
 const addPhoneToInventory = (inventory, phoneObject, price) => {
     inventory.set(phoneObject, price);
@@ -27,7 +33,7 @@ let inventory = new Map()
 // save price in new variable
 // delete price property
 // set phone and price to inventory map using function
-for (const phone of phones){
+for (const phone of phones) {
     const price = phone.price
     delete phone.price
     inventory = addPhoneToInventory(inventory, phone, price)
